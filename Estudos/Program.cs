2ce9e03ada6a098517fa7d241cc8be9b317b64fe
dansoares8 <danielsoares8296@gmail.com>
2024-11-27 20:42:17 -0300

    up

--- Estudos/Program.cs
+++ Estudos/Program.cs
@@ -1,2 +1,74 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+//Console.WriteLine("Hello, World!");
+
+
+//Char (Caractere):
+// Console.WriteLine('b');
+
+//Char (caractere) só é possível inserir um caractere, as aspas simples sinalizam o tipo char:
+// Console.WriteLine('Hello World!');
+
+//Inteiro (int):
+//Console.WriteLine(123);
+
+//Tipo float (ponto flutuante):
+//Console.WriteLine(0.25F);
+
+//Tipo double:
+//Console.WriteLine(2.625);
+
+//Tipo decimal:
+//Console.WriteLine(12.39816m);
+
+//Tipo boolean (bool):
+//Console.WriteLine(true);
+//Console.WriteLine(false);
+
+//declarando uma variável e atribuindo valor a ela de forma correta:
+//string firstName;
+//firstName = "Bob";
+
+//jeito errado de atribuir valor a uma variável:
+//string firstName;
+//"Bob" = firstName;
+
+//não pode declarar um tipo de dado Int e atribuir um valor string, diferente do tipo de dado da variável.
+//int firstName;
+//firstName = "Bob";
+
+//string firstName;
+//firstName = "Bob";
+//Console.WriteLine(firstName);
+
+/*
+string firstName;
+firstName = "Bob";
+Console.WriteLine(firstName);
+firstName = "Liem";
+Console.WriteLine(firstName);
+firstName = "Isabela";
+Console.WriteLine(firstName);
+firstName = "Yasmin";
+Console.WriteLine(firstName);
+*/
+
+//antes de recuperar o valor da variável, você deve atribuir um valor a ela:
+//string firstName;
+//Console.WriteLine(firstName);
+
+//definir variáveis de tipo implícito:
+//var message = "Hello World!";
+//message = 10.703m;
+//gera um erro pq não é possível alterar o tipo de dado de uma variável var,
+//assim que é criada, ela continuará com o mesmo tipo de dado da primeira declaração.
+
+//string nome = "Bob";
+//int numero = 3;
+//decimal temperatura = 34.4m;
+//Exercício prático:
+//Console.WriteLine("Hello, " + nome + "! You have " + numero + " messages in your inbox. The temperature is " + temperatura + " celsius.");
+
+
+
+
+
